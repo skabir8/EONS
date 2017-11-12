94be07de5851863f461d747732990aa342858587
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2017-11-11 19:30:27 -0500

    added complaint page

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return render_template('home.html')
+
+@app.route("/map")
+def map():
+    return render_template("map2.html")
+
+@app.route("/comp")
+def compform():
+    return render_template('complaintform.html')
 
 
 app.run(debug=True,host="0.0.0.0",port=5000)
