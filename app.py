0f89566b73bf218c97f59d6c84d77eec4bfe008d
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2017-11-11 19:46:19 -0500

    complaint form created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,9 +9,13 @@ def home():
 def map():
     return render_template("map2.html")
 
-@app.route("/comp")
+@app.route("/comp", methods=['GET','POST'])
 def compform():
-    return render_template('complaintform.html')
-
+    print request.method
+    if request.method=="GET":
+        return render_template('complaintform.html')
+    if request.method=="POST":
+        
+        return "Complaint Submitted"
 
 app.run(debug=True,host="0.0.0.0",port=5000)
