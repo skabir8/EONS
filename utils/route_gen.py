0c8b0b656c86b6ebe7a33d42834a0631da43b94c
skabir8 <shariar.k8@gmail.com>
2017-11-11 19:47:34 -0500

    finished routegen

--- utils/route_gen.py
+++ utils/route_gen.py
@@ -16,8 +16,11 @@ def get_routes(origin,destination):
     base += origin + destination + '&mode=walking&alternatives=on&key=AIzaSyCrIErnSCAoA07R_m9NqNIQNcuD0Cf3Vn4'
     data = requests.get(base).text
     dic = json.loads(data)
-    print(base)
-    return dic
-
+    poly_line = (dic['routes'])[0]['overview_polyline']['points']
+    poly_line_array = polyline.decode(poly_line)
+    ret_val = ''
+    for vals in poly_line_array:
+        ret_val += '{lat: ' + str(vals[0]) + ',' + 'lng: ' + str(vals[1])+'}, '
+    return ret_val[:-2]
 #print (get_routes("74-39 Jamaica Ave, Woodhaven, NY 11421", "C-Town Supermarkets, 241 Taaffe Pl, Brooklyn, NY 11205"))
-print(polyline.decode('''mpjwFn`zaM`AzKvB`Nj@vCPp@dFxOvAhEcGxF}CxCUTY`@Yh@Qb@OdAEtALjADzAAn@Ed@Mr@Sr@o@`Bo@tAk@rA_@j@SX?JDRDNL\\bEl[t@bGAVmAjGcBxIyCpOqBpEaChFu@rAiCjD}GpIgB~BgDxGwAjCqApCgD|GaCvEc@~@N|Cd@rIf@xIFbAJ|@VrFXdHRxDlAzUjCri@Bf@dKxJjEhEUh@rGzF\\fHnChf@x@lNjIryApB|]gBT'''))
+#print(polyline.decode('''mpjwFn`zaM`AzKvB`Nj@vCPp@dFxOvAhEcGxF}CxCUTY`@Yh@Qb@OdAEtALjADzAAn@Ed@Mr@Sr@o@`Bo@tAk@rA_@j@SX?JDRDNL\\bEl[t@bGAVmAjGcBxIyCpOqBpEaChFu@rAiCjD}GpIgB~BgDxGwAjCqApCgD|GaCvEc@~@N|Cd@rIf@xIFbAJ|@VrFXdHRxDlAzUjCri@Bf@dKxJjEhEUh@rGzF\\fHnChf@x@lNjIryApB|]gBT'''))
